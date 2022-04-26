df00b55c62fbafefdc9541c8fe75f08ecfabf998
Okeke Fabian <freebornprince@yahoo.com>
2022-04-26 14:11:55 +0200

    fixed stuffs

---
+++ djangoportfolio/portfolio/views.py
@@ -0,0 +1,13 @@
+from json import load
+from re import template
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+    template = loader.get_template('portfolio/index.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
